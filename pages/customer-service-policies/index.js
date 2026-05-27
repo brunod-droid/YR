defd6727af5d82ba606076f352ac981cc467099e
brunod-droid <bruno.d@tenengroup.com>
2026-05-27 14:29:51 +0300

    Update index.js

--- pages/customer-service-policies/index.js
+++ pages/customer-service-policies/index.js
@@ -1,1 +1,13 @@
+import { useEffect } from "react";
 
+export default function PoliciesRedirect() {
+  useEffect(() => {
+    window.location.href = "/customer-service-policies";
+  }, []);
+
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial, sans-serif" }}>
+      Redirecting to Customer Service Policies...
+    </div>
+  );
+}
