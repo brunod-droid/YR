8606fea8c519be4c8f3209fda9df785b74fd5d0f
brunod-droid <bruno.d@tenengroup.com>
2026-05-27 13:35:48 +0300

    Add files via upload

--- pages/policies/index.js
+++ pages/policies/index.js
@@ -1,1 +1,13 @@
 
+export default function Policies() {
+  return (
+    <div style={{ padding:40 }}>
+      <h1>Yves Rocher Customer Service Policies</h1>
+      <div style={{ marginTop:20 }}>
+        <div>Refund policy</div>
+        <div>Shipping delays</div>
+        <div>Escalation process</div>
+      </div>
+    </div>
+  );
+}
