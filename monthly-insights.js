eef7ba438aa4c57fbe63332de2710baf8530785d
brunod-droid <bruno.d@tenengroup.com>
2026-07-15 13:34:37 +0200

    clean repository

--- monthly-insights.js
+++
@@ -1,2 +0,0 @@
-import WeeklyItemsPage from "../../lib/yr-reporting/WeeklyItemsPage";
-export default function DevelopmentsPage() { return <WeeklyItemsPage type="developments" />; }
--- monthly-upload.js
+++
@@ -1,2 +0,0 @@
-import WeeklyItemsPage from "../../lib/yr-reporting/WeeklyItemsPage";
-export default function FuturePlansPage() { return <WeeklyItemsPage type="futurePlans" />; }
